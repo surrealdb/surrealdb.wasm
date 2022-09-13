fe162ee32b7b50985dbff1c6f5fc7146084c4049
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-09-14 00:16:31 +0100

    Add initial Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+export default [
+	{
+		target: 'web',
+		mode: 'development',
+		entry: '/index.js',
+		output: {
+			clean: true,
+			filename: 'index.js',
+			globalObject: 'window',
+			path: new URL('./dist/web', import.meta.url).pathname,
+			library: {
+				name: 'Surreal',
+				type: 'window',
+				export: 'default',
+				umdNamedDefine: true,
+			},
+		},
+		devServer: {
+			compress: true,
+			client: {
+				overlay: {
+					errors: true,
+					warnings: true,
+				},
+			},
+			static: {
+				directory: process.cwd(),
+			},
+			watchFiles: [
+				'index.html',
+				'index.js',
+				'pkg/*',
+			],
+		},
+		experiments: {
+			syncWebAssembly: true,
+		},
+		devtool: false,
+	},
+	{
+		target: 'node',
+		mode: 'production',
+		entry: './index.js',
+		output: {
+			clean: true,
+			filename: 'index.cjs',
+			path: new URL('./dist/lib', import.meta.url).pathname,
+			library: {
+				name: 'Surreal',
+				type: 'umd',
+				export: 'default',
+				umdNamedDefine: true,
+			},
+		},
+		experiments: {
+			syncWebAssembly: true,
+		},
+		devtool: false,
+	},
+];
