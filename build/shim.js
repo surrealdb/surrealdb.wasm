cf5b0a783931b05df6dc080c18c6edf5265c489b
Micha de Vries <micha@devrie.sh>
2023-06-19 10:43:11 +0200

    Add some comments

--- build/shim.js
+++ build/shim.js
@@ -1,3 +1,6 @@
+// We need a file reading API that works with NodeJS. This API is not used in the browser, but is used in Deno and Node.
+// Use the global Deno variable if already available
+// Otherwise create a custom object for the readFile function which uses Node's "fs" api under the hood.
 const Deno =
 	globalThis.Deno ?? (
 		typeof global === 'undefined'
@@ -7,6 +10,7 @@ const Deno =
 			}
 	)
 
+// Provide a custom require function to the WASM engine. If the crypto module is not available globally, we need to pass it though manually
 const module = await (async () => {
 	const crypto = globalThis.crypto ?? await import('node:crypto');
 	return {
