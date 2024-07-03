4fd1a167e5582c9aa8da6a2e6803ebfecde2827f
Micha de Vries <micha.de.vries@surrealdb.com>
2024-07-03 13:48:12 +0200

    Micha/improve version logic (#90)

--- extract-surrealdb-version.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-VERSION="$( cargo metadata --format-version=1 --no-deps | jq -c '.packages[0].dependencies[] | select( .name == "surrealdb" ) | .req | match("^\\^?(.*)"; "i") | .captures[0].string' | sed 's:^.\(.*\).$:\1:' )"
-
-echo -n $VERSION > src/surrealdb-version
